8e6249e05c458afb79b23726694b0d3d50d2a78e
abirgharbi <abirgharbi046@gmail.com>
2025-05-09 12:30:06 +0100

    merging

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ import authRoute from "./routes/authRouter";
 import customerRoute from "./routes/customersRouter";
 import categoryRoute from "./routes/category";
 import productRoute from "./routes/product";
+import path from 'path';
 
 const app = express();
 
-app.use(express.json());
+// Serving assetlinks.json manually
+app.get('/.well-known/assetlinks.json', (req, res) => {
+  const assetPath = path.join(__dirname, '..', 'public', '.well-known', 'assetlinks.json');
+  console.log('Serving assetlinks.json from:', assetPath);
+  res.sendFile(assetPath);
+});
 
 app.use(
   cors({
@@ -26,18 +32,14 @@ app.use(
   })
 );
 
-
 // routes
 app.use("/user", authRoute); // auth route
 app.use("/user/customer", customerRoute);
 app.use("/product/category", categoryRoute);
-app.use("/product", productRoute); 
-
-
+app.use("/product", productRoute);
 
 app.listen(4002, () =>
   console.log(`server running on port : ${process.env.PORT} \n`)
 );
-
 
 export default app;
